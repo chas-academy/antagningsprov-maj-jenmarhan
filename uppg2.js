c8eb5d4f63c7ad53fb7f79226f12b7abf119b2bc
jenmarhan <jennifermhansson@gmail.com>
2025-05-08 21:10:28 +0200

    Update uppg2.js

--- uppg2.js
+++ uppg2.js
@@ -1,6 +1,11 @@
+
+
+function uppg2(){
 public class MittNamn {
     public static void main(String[] args) {
         String namn = "Jennifer";
         System.out.println("Mitt namn är " + namn);
     }
 }
+
+module.exports = { uppg2 };
