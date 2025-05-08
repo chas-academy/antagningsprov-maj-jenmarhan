38d1f617487a179a752098310a704be15a693760
jenmarhan <jennifermhansson@gmail.com>
2025-05-08 23:01:44 +0200

    Update uppg7.js

--- uppg7.js
+++ uppg7.js
@@ -2,9 +2,26 @@
 
 function uppg7() {
    
-    // skapa en funktion som tar in två tal som argument
-    // funktionen ska returnera summan av talen
-    // kalla på funktionen i en return statement och ge den parametrarna 5 och 10
+   import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Ange ett heltal ");
+        int tal1 = input.nextInt();
+
+        System.out.print("Ange ett till heltal: ");
+        int tal2 = input.nextInt();
+        int summa = summera(tal1, tal2);
+
+        System.out.println("Summan av talen är " + summa);
+    }
+
+    public static int summera(int a, int b) {
+        return a + b;
+    }
+}
 
  
 }
